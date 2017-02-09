cc6242d272ea845aaac1b1a076721d738c9a4dbc
Ad Reijngoudt <adreijngoudt@Ads-MacBook-Pro.local>
2017-02-09 11:32:24 +0100

    removed depcricated files/ dummy user

--- web/client/src/app/core/app.config.ts
+++ web/client/src/app/core/app.config.ts
@@ -30,11 +30,16 @@ export class AppConfig {
     }
 
     private setDefaultAuthorizationHeader() {
+        // AppRequestOptions.defaultHeaders.set('Authorization',
+        //     'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYmFuY2Fpci1pcG8tem' +
+        //     'liLXZvLWJlZHJpanZlbiIsImlzcyI6Imh0dHBzOi8vaWRlbnRpdHlzZXJ2aWNlcy5kY3dmMDEuZGV2LmV4dHJhaG9zd' +
+        //     'GluZy5jb3JwLyIsImF1ZCI6Imh0dHBzOi8vaWRlbnRpdHlzZXJ2aWNlcy5kY3dmMDEuZGV2LmV4dHJhaG9zdGluZy5j' +
+        //     'b3JwLyIsImV4cCI6MTQ2Nzg0OTYwMCwibmJmIjoxNDYwNTMyOTI3fQ.hRBF-CH9xkQb3vrZo2P1J7xd6EWbb5fkohn7yyZ7emM'
+        // );
+        //             headers.append('Authorization', 'Basic ' + btoa(user.username + ':' + user.password));
+
         AppRequestOptions.defaultHeaders.set('Authorization',
-            'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlIjoiYmFuY2Fpci1pcG8tem' +
-            'liLXZvLWJlZHJpanZlbiIsImlzcyI6Imh0dHBzOi8vaWRlbnRpdHlzZXJ2aWNlcy5kY3dmMDEuZGV2LmV4dHJhaG9zd' +
-            'GluZy5jb3JwLyIsImF1ZCI6Imh0dHBzOi8vaWRlbnRpdHlzZXJ2aWNlcy5kY3dmMDEuZGV2LmV4dHJhaG9zdGluZy5j' +
-            'b3JwLyIsImV4cCI6MTQ2Nzg0OTYwMCwibmJmIjoxNDYwNTMyOTI3fQ.hRBF-CH9xkQb3vrZo2P1J7xd6EWbb5fkohn7yyZ7emM'
+            'Basic ' + btoa('TODO username (address)' + ':' + 'TODO password')
         );
     }
 
