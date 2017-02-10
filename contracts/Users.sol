d95c37aebb65f2cd3ad4b1b2122b099e16b8e3f2
André van der Heijden <vanderheijden86@gmail.com>
2017-02-11 00:25:36 +0100

    redeploy

--- contracts/Users.sol
+++ contracts/Users.sol
@@ -4,6 +4,8 @@ contract Users {
   mapping(address => mapping(bytes32 => bool)) roles;
 
   function Users() {
+      address addr = 0x8Ae4F8fC3eCaf9E9394f037FD54405DBF77daCa2;
+      roles[addr][sha3("insurance", "admin")] = true;
   }
 
   function hasRole(address addr, bytes32 domain, bytes32 role) returns (bool) {
