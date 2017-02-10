c7994c2f0b6b3204a88e1dbc2a593ea25444a3d9
Ad Reijngoudt <adreijngoudt@Ads-MacBook-Pro.local>
2017-02-10 20:28:44 +0100

    .

--- web/client/src/app/+proposal/overview/proposal-overview.component.ts
+++ web/client/src/app/+proposal/overview/proposal-overview.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'proposal-overview',
+    selector: 'app-proposal-overview',
     templateUrl: './proposal-overview.component.html',
     styleUrls: ['./proposal-overview.component.scss'],
 })
