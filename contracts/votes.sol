29ba77e8786902943e6258eec3b2882eb2e8413d
André van der Heijden <vanderheijden86@gmail.com>
2017-02-10 21:30:08 +0100

    updated erd

---
+++ contracts/votes.sol
@@ -0,0 +1,15 @@
+contract Votes {
+
+    struct Vote {
+        uint userId;
+        uint proposalId;
+        uint value;
+        bool voteRevoked;
+        string comment;
+        uint timestamp;
+    }
+
+
+
+
+}
