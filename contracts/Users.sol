d2e518f463dcfa1be635c91d47e3d1fc2a2a4777
Pascal van Kooten <kootenpv@gmail.com>
2017-02-10 21:36:16 +0100

    Users.sol added

---
+++ contracts/Users.sol
@@ -0,0 +1,21 @@
+contract Users {
+
+  mapping(address => mapping(bytes32 => bool)) roles;
+
+  function Users() {
+  }
+
+  function hasRole(address addr, bytes32 domain, bytes32 role) returns (bool) {
+    return roles[addr][sha3(domain, role)];
+  }
+
+  function setRole(address addr, bytes32 domain, bytes32 role, bool state) returns (bytes32){
+    // check if admin. not in hackathon :)!
+    // if (!hasRole(tx.origin, "admin")) {
+    //    return "setRole requires 'admin' role";
+    //}
+    roles[addr][sha3(domain, role)] = state;
+    return "Role has been set";
+  }
+
+}
