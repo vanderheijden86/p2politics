65eca47bb7f2ae93889abc182ceacc18cc37d2ed
Pascal van Kooten <kootenpv@gmail.com>
2017-02-11 21:18:34 +0100

    add_votes.js

--- misc/add_votes.js
+++ misc/add_votes.js
@@ -18,7 +18,7 @@ module.exports = function(callback) {
 
     Votes.deployed().then(function(instance) {
         votes = instance;
-        return Votes.vote(VotesItem.proposalId, VotesItem.value, VotesItem.comment); // {from: account_one});
+        return Votes.voteAs(VotesItem.userAddress, VotesItem.proposalId, VotesItem.iteration, VotesItem.value, VotesItem.comment); // {from: account_one});
     }).then(function(result) {
         // If this callback is called, the transaction was successfully processed.
         console.log("Transaction successful!", result);
