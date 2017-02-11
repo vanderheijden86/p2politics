a9b99a06256a1785c460583ec7fe3471b2182a24
Remi <remi@Remis-MacBook-Pro.local>
2017-02-11 20:53:44 +0100

    Set service function call addProposal (not tested)

--- web/client/src/app/+proposal/add/proposal-add.component.ts
+++ web/client/src/app/+proposal/add/proposal-add.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
+import { MdSnackBar } from '@angular/material';
 import { Subscription } from 'rxjs/Subscription';
 
+import { ProposalService } from '../../services/proposal.service';
 import { Proposal } from '../../models/proposal.model';
 
 @Component({
@@ -11,18 +13,23 @@ import { Proposal } from '../../models/proposal.model';
     styleUrls: ['./proposal-add.component.scss']
 })
 export class ProposalAddComponent implements OnInit, OnDestroy {
-    parentProposalId: string;
 
     formGroup: FormGroup;
 
+    private domain: string;
+    private parentProposalId: string;
     private routeSubscription: Subscription;
 
-    constructor(private route: ActivatedRoute) { }
+    constructor(
+        private route: ActivatedRoute,
+        private mdSnackBar: MdSnackBar,
+        private proposalService: ProposalService) { }
 
     ngOnInit() {
         this.initForm();
         this.routeSubscription = this.route.params.subscribe(params => {
-            console.log(params);
+            console.log('ADD PROPOSAL PARAMS', params);
+            this.domain = params['domain'];
             this.parentProposalId = params['parentId'];
         });
     }
@@ -33,7 +40,15 @@ export class ProposalAddComponent implements OnInit, OnDestroy {
 
     onAddProposal() {
         if(this.formGroup.invalid) return;
-
+        const formValues = this.formGroup.value;
+        const proposal: Proposal = Object.assign(new Proposal(), formValues);
+        proposal.domain = this.domain;
+        this.proposalService.addProposal(proposal)
+            .subscribe(() => {
+                this.mdSnackBar.open('Het nieuwe voorstel wordt aangemaakt', 'Sluiten', {
+                    duration: 8000
+                });
+            });
     }
 
     private initForm() {
@@ -41,9 +56,8 @@ export class ProposalAddComponent implements OnInit, OnDestroy {
             'title': new FormControl(undefined, Validators.required),
             'description': new FormControl(undefined, Validators.required),
             'category': new FormControl(undefined, Validators.required),
-            'domain': new FormControl(undefined, Validators.required),
             'phase': new FormControl(undefined),
-            'enddate': new FormControl(undefined, Validators.required)
+            'endDate': new FormControl(undefined, Validators.required)
         });
     }
 }
