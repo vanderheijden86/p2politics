e06567881fd578f4d125ed9b94fe02a0a8adb27a
André van der Heijden <vanderheijden86@gmail.com>
2017-02-11 11:45:26 +0100

    redeploy

--- contracts/Users.sol
+++ contracts/Users.sol
@@ -6,17 +6,17 @@ contract Users {
   function Users() {
      }
 
-  function hasRole(address addr, bytes32 domain, bytes32 role) constant returns (uint, address) {
-      return (roles[addr][sha3(domain, role)], addr);
+  function hasRole(address addr, bytes32 domain, bytes32 role) constant returns (uint) {
+      return roles[addr][sha3(domain, role)];
   }
 
-  function setRole(address addr, bytes32 domain, bytes32 role, uint state) returns (bytes32, address) {
+  function setRole(address addr, bytes32 domain, bytes32 role, uint state) returns (bytes32){
     // check if admin. not in hackathon :)!
     // if (!hasRole(tx.origin, domain, "admin")) {
     //    return "setRole requires 'admin' role";
     //}
     roles[addr][sha3(domain, role)] = state;
-    return ("Role has been set", addr);
+    return "Role has been set";
   }
 
   function testje() returns (uint) {
--- misc/set_role.js
+++ misc/set_role.js
@@ -5,7 +5,7 @@ var Users = artifacts.require("../contracts/Users.sol");
 
     var account_one = "0x731e2a7023ba0064805c673d7a171787a5fda8a5"; // an address
     var account_two = "0x8b63b4f484623879dfd924e28469cbcb1b620eaf"; // another address
-    var addr = 0x8Ae4F8fC3eCaf9E9394f037FD54405DBF77daCa2;
+    var addr = "0x8Ae4F8fC3eCaf9E9394f037FD54405DBF77daCa2";
     var domain = "insurance";
     var role = "admin";
 
