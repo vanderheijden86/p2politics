171a7faba37dd746ffece2500dda73fe8406c8c6
Remi <remi@Remis-MacBook-Pro.local>
2017-02-11 10:34:55 +0100

    Start on proposal detail view

--- web/client/src/app/+proposal/detail/proposal-detail.component.ts
+++ web/client/src/app/+proposal/detail/proposal-detail.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Proposal } from '../../models/proposal.model';
 
 @Component({
     selector: 'app-proposal-detail',
     templateUrl: './proposal-detail.component.html',
     styleUrls: ['./proposal-detail.component.scss'],
 })
-export class ProposalDetailComponent {
+export class ProposalDetailComponent implements OnInit, OnDestroy {
+    proposal: Proposal;
 
+    ngOnInit() {
+        this.proposal = <any>{
+            title: 'Test title for proposal',
+            description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                                Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.`
+        };
+    }
+
+    ngOnDestroy() {
+
+    }
 }
