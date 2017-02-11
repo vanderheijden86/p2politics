65f47e222575c885bd378930cfdc44319a13bf4c
Remi <remi@Remis-MacBook-Pro.local>
2017-02-11 08:29:03 +0100

    Tab indent

--- web/client/src/app/+proposal/overview/proposal-overview.component.ts
+++ web/client/src/app/+proposal/overview/proposal-overview.component.ts
@@ -21,11 +21,11 @@ export class ProposalOverviewComponent implements OnInit, OnDestroy {
     proposalStub:Proposal[] = [
         <any>{ id: '123', title: 'Title one', category: 'Natuur en Ruimtelijke Ordening',
                 description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.`
+                                Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.`
             },
         <any>{ id: '456', title: 'Title two', category: 'Natuur en Ruimtelijke Ordening',
                 description: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
-Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.`
+                                Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim.`
             },
         <any>{ id: '789', title: 'Title three', category: 'Veiligheid' }
     ];
