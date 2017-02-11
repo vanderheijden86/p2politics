350958f23e8f92594abd92addfc0d8fd63a4fc87
Pascal van Kooten <kootenpv@gmail.com>
2017-02-11 20:06:01 +0100

    fixed

--- contracts/Proposals.sol
+++ contracts/Proposals.sol
@@ -13,42 +13,38 @@ contract Proposals {
     bytes32 category;
     bytes32 phase;
     string description;
-    uint maxVoteScale;
     uint startDate;
     uint endDate;
     uint completed;
   }
 
   function Proposals() {
-    setProposal(proposalCount, "Winkelcentrumpje", "local", "category", "phase", "description", 100, now + 2 days, 0);
+      proposalDepths[0] = 0;
+    newProposal("Winkelcentrumpje", "local", "category", "phase", "description",  now + 2 days, 0);
   }
 
   function newProposal(bytes32 title, bytes32 domain, bytes32 category, bytes32 phase,
-                       string description, uint maxVoteScale, uint endDate, uint completed) returns (uint) {
-    proposals.push(Proposal(proposalCount, proposalDepths[proposalCount], title, domain, category, phase, description, maxVoteScale, now, endDate, completed));
-    proposalDepths[proposalCount]++;
-    proposalCount++;
+                       string description, uint endDate, uint completed) returns (uint) {
+    _setProposal(proposalCount, title, domain, category, phase, description, endDate, completed);
     return proposalCount;
   }
 
   function newIteration(uint id, bytes32 title, bytes32 domain, bytes32 category, bytes32 phase,
-                       string description, uint maxVoteScale, uint endDate, uint completed) returns (uint) {
-    proposals.push(Proposal(id, proposalDepths[id], title, domain, category, phase, description, maxVoteScale, now, endDate, completed));
-    proposalDepths[id]++;
-    proposalCount++;
+                       string description, uint endDate, uint completed) returns (uint) {
+    _setProposal(id, title, domain, category, phase, description, endDate, completed);
     return proposalCount;
   }
 
-  function getProposal(uint index) constant returns (uint, uint, bytes32, bytes32, bytes32, bytes32, string, uint, uint, uint, uint) {
+  function getProposal(uint index) constant returns (uint, uint, bytes32, bytes32, bytes32, bytes32, string, uint, uint, uint) {
     Proposal p = proposals[index];
-    return (p.id, p.iteration, p.title, p.domain, p.category, p.phase, p.description, p.maxVoteScale, p.startDate, p.endDate, p.completed);
+    return (p.id, p.iteration, p.title, p.domain, p.category, p.phase, p.description, p.startDate, p.endDate, p.completed);
   }
 
-  function getProposalByIdIteration(uint id, uint iteration) constant returns (uint, uint, bytes32, bytes32, bytes32, bytes32, string, uint, uint, uint, uint) {
+  function getProposalByIdIteration(uint id, uint iteration) constant returns (uint, uint, bytes32, bytes32, bytes32, bytes32, string, uint, uint, uint) {
     for (uint i = 0; i < proposalCount; i++ ) {
       if (proposals[i].id == id && proposals[i].iteration == iteration) {
         Proposal p = proposals[i];
-        return (p.id, p.iteration, p.title, p.domain, p.category, p.phase, p.description, p.maxVoteScale, p.startDate, p.endDate, p.completed);
+        return (p.id, p.iteration, p.title, p.domain, p.category, p.phase, p.description, p.startDate, p.endDate, p.completed);
       }
     }
   }
@@ -57,4 +53,15 @@ contract Proposals {
     return proposalCount;
   }
 
+  function _setProposal(uint id, bytes32 title, bytes32 domain, bytes32 category, bytes32 phase,
+                        string description, uint endDate, uint completed) returns (uint) {
+    proposals.push(Proposal(id, _getDepth(id), title, domain, category, phase, description, now, endDate, completed));
+    proposalDepths[id]++;
+    proposalCount++;
+    return proposalCount;
+  }
+
+  function _getDepth(uint id) internal constant returns (uint) {
+    return proposalDepths[id];
+  }
 }
