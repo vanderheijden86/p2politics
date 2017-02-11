7f4f2f26d7432a75792ac72c1bb39b7f9cddf804
André van der Heijden <vanderheijden86@gmail.com>
2017-02-11 11:36:29 +0100

    redeployed

--- contracts/Users.sol
+++ contracts/Users.sol
@@ -6,17 +6,17 @@ contract Users {
   function Users() {
      }
 
-  function hasRole(address addr, bytes32 domain, bytes32 role) constant returns (uint) {
-      return roles[addr][sha3(domain, role)];
+  function hasRole(address addr, bytes32 domain, bytes32 role) constant returns (uint, address) {
+      return (roles[addr][sha3(domain, role)], addr);
   }
 
-  function setRole(address addr, bytes32 domain, bytes32 role, uint state) returns (bytes32){
+  function setRole(address addr, bytes32 domain, bytes32 role, uint state) returns (bytes32, address) {
     // check if admin. not in hackathon :)!
     // if (!hasRole(tx.origin, domain, "admin")) {
     //    return "setRole requires 'admin' role";
     //}
     roles[addr][sha3(domain, role)] = state;
-    return "Role has been set";
+    return ("Role has been set", addr);
   }
 
   function testje() returns (uint) {
